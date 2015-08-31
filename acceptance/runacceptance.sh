46cbee0c2d64dc2b9c2489dd06fc04b3404c8df3
Trent Ford <trentford@Trents-MacBook-Air.local>
2015-08-31 16:04:39 -0500

    Updated

--- acceptance/runacceptance.sh
+++ acceptance/runacceptance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 javac -cp .:../build/classes/main HelloWorldKeywords.java
-java -cp .:../build/classes/main:/home/agarrett/RobotFramework/robotframework-2.9.jar org.robotframework.RobotFramework HelloWorldTests.txt
+java -cp .:../build/classes/main:/Users/trentford/Documents/Software_Engineering_1_Class/Programs/robotframework-2.9.jar org.robotframework.RobotFramework HelloWorldTests.txt
 
